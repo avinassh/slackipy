c7c9d958aada2e0fb64009dbf3fb64b1f69ab245
avi <hi@avi.im>
2015-11-07 21:33:06 +0530

    Add `main.py`

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_wtf.csrf import CsrfProtect
+
+from views import Index
+
+
+app = Flask(__name__)
+CsrfProtect(app)
+app.add_url_rule('/', view_func=Index.as_view('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
