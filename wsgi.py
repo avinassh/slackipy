5f853d6d6db4fbff788ff5d6d0d9e82d1f7abfd3
avi <hi@avi.im>
2015-11-07 21:57:44 +0530

    Update `wsgi.py` for Openshift specific code

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,19 @@
 
 import os
 
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    exec_namespace = dict(__file__=virtualenv)
+    with open(virtualenv, 'rb') as exec_file:
+        file_contents = exec_file.read()
+    compiled_code = compile(file_contents, virtualenv, 'exec')
+    exec(compiled_code, exec_namespace)
+except IOError:
+    pass
+
+
 from main import app as application
 
 application.config['slack_team_name'] = os.environ['slack_team_name']
