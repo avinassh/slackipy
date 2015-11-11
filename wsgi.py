657b9cd09b3d4c7c07f53bc8f2d995d3b08b63c5
avi <hi@avi.im>
2015-11-11 14:53:37 +0530

    Use `0.0.0.0` instead of `localhost`

--- wsgi.py
+++ wsgi.py
@@ -25,6 +25,5 @@ application.config['SECRET_KEY'] = os.environ['FLASK_SECRET_KEY']
 # Below for testing locally only
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
-    # Wait for a single request, serve it and quit.
+    httpd = make_server('0.0.0.0', 8051, application)
     httpd.serve_forever()
