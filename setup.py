b442c2291d723995ce4c0cd688e3b586b24c7509
Andrej Rode <mail@andrejro.de>
2018-01-19 13:59:54 +0100

    move templates & static and include in package_data

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup
+import os
+
+def get_data_files(mypkg, dirs):
+    data_files = []
+    for folder in dirs:
+        start_point = os.path.join(mypkg, folder)
+        for root, dirs, files in os.walk(start_point):
+            data_files.extend([os.path.join(*(root.split(os.path.sep)[1:]),i) for i in files])
+    return data_files
 
 setup(name='slackipy',
       version='0.1',
@@ -6,5 +15,5 @@ setup(name='slackipy',
       author='Avinash Sajjanshetty',
       author_email='hi@avi.im',
       packages = ['slackipy'],
-      package_data = {'slackipy': ['static/*', 'templates/*']},
+      package_data = {'slackipy': get_data_files('slackipy', ['static','templates']) },
       url='https://github.com/avinassh/slackipy')
--- slackipy/__init__.py
+++ slackipy/__init__.py
@@ -1,13 +1,16 @@
+import os
+
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 from .views import IndexView
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(__path__[0], 'templates'))
 CSRFProtect(app)
 app.add_url_rule('/', view_func=IndexView.as_view('index'))
-app.config.from_envvar('SLACKIPY_CONFIG')
+
+if os.environ.get('SLACKIPY_CONFIG', None) is not None:
+    app.config.from_envvar('SLACKIPY_CONFIG')
 
 
 if __name__ == '__main__':
